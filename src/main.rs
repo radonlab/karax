7e288c33f784295c2d348213607c14b695de8ece
skylerlee <skyler.ac.lee@gmail.com>
2025-10-22 23:42:39 +0800

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // Don't link the Rust standard library
+#![no_main] // Disable all Rust-level entry points
+
+use panic_halt as _;
+use cortex_a_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    // Diverging main function never returns
+    loop {}
 }
