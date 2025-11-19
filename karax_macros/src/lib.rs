37b4becdd30541bc9062dd84da1cba1825fec9de
skylerlee <skyler.ac.lee@gmail.com>
2025-11-19 23:50:06 +0800

    feat: add entry macro

--- karax_macros/src/lib.rs
+++ karax_macros/src/lib.rs
@@ -3,8 +3,19 @@
 // found in the LICENSE file.
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{ItemFn, parse_macro_input};
 
 #[proc_macro_attribute]
-pub fn entry(args: TokenStream, input: TokenStream) -> TokenStream {
-    quote!().into()
+pub fn entry(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let main_fn = parse_macro_input!(input as ItemFn);
+    let ident = &main_fn.sig.ident;
+    quote! {
+        #main_fn
+
+        pub unsafe extern "C" fn kmain() -> ! {
+            #ident()
+        }
+    }
+    .into()
 }
