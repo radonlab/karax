1422c6f9fc8cede934586035efe4e159b96a1ea5
skylerlee <skyler.ac.lee@gmail.com>
2025-11-26 00:57:36 +0800

    feat: update macros

--- karax_macros/src/lib.rs
+++ karax_macros/src/lib.rs
@@ -4,12 +4,34 @@
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{ItemFn, parse_macro_input};
+use syn::{parse_macro_input, Error, ItemFn};
+
+fn validate_fn_signature(fn_sig: &syn::Signature) -> Result<(), Error> {
+    if fn_sig.inputs.len() != 0 {
+        return Err(Error::new_spanned(
+            fn_sig,
+            "The entry function should take no arguments",
+        ));
+    }
+
+    if matches!(fn_sig.output, syn::ReturnType::Default) {
+        return Err(Error::new_spanned(
+            fn_sig,
+            "The entry function should return ! (never type)",
+        ));
+    }
+
+    Ok(())
+}
 
 #[proc_macro_attribute]
 pub fn entry(_args: TokenStream, input: TokenStream) -> TokenStream {
     let main_fn = parse_macro_input!(input as ItemFn);
     let ident = &main_fn.sig.ident;
+    // check the function signature
+    if let Err(error) = validate_fn_signature(&main_fn.sig) {
+        return error.to_compile_error().into();
+    }
     quote! {
         #main_fn
 
